59c7006a41a070369f4384d0f9fc4012bb0fcaf5
Jonathan Nicol <jonathan@f6design.com>
2013-03-26 16:43:28 +1100

    Added a demo.

---
+++ demo.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once('StandalonePHPEnkoder.php');
+$enkoder = new Enkoder();
+$html = 'Why not <a href="mailto:test@test.com?subject=Hello" tile="Email me">email me</a>. Your email will be sent to test@test.com. Here is another mailto, with an email address as the anchor text: <a href="mailto:test@test.com">test@test.com</a>. If you view source you shouldn\'t see any email addresses.';
+echo $enkoder->enkodeAllEmails($html);
+
+?>
